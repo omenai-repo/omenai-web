c689f72896ba8dc9158ff75edd3991f756c24e7b
Moses Chukwunekwu <moses@omenai.net>
2024-07-22 21:27:11 +0100

    Fix

--- utils/priceFormatter.ts
+++ utils/priceFormatter.ts
@@ -1,5 +1,6 @@
 export function formatPrice(price: number, currency?: string): string {
-  const new_price = Number(price.toFixed(1));
+  const priceNumber = Number(price);
+  const new_price = Number(priceNumber.toFixed(1));
   if (currency) {
     return `${currency}${new_price.toLocaleString()}`;
   } else {
